31d52174849fe0e335e66ee5b4fe799416068d1d
tiescl <anvar.madvaliev@icloud.com>
2024-09-30 18:39:45 +0500

    feat: implement infinite querying

--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -1,7 +1,7 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import {
     useQueryClient,
-    useQuery,
+    useInfiniteQuery,
     useMutation
 } from "@tanstack/react-query";
 import { createMockPost, fetchMockPosts } from "../data";
@@ -14,12 +14,18 @@ export function Posts({
     const queryClient = useQueryClient();
     const [title, setTitle] = useState("");
     const [body, setBody] = useState("");
+    const ref = useRef(null);
 
-    const {
-        data: posts,
-        isLoading,
-        isError
-    } = useQuery({ queryKey: ["posts"], queryFn: fetchMockPosts });
+    const { data, isLoading, isError, fetchNextPage, hasNextPage } =
+        useInfiniteQuery({
+            queryKey: ["posts"],
+            queryFn: fetchMockPosts,
+            initialPageParam: 0,
+            getNextPageParam: function (lastPage) {
+                var next = lastPage.nextIdx;
+                return next == -1 ? undefined : next;
+            }
+        });
 
     const mutation = useMutation({
         mutationFn: createMockPost,
@@ -28,6 +34,32 @@ export function Posts({
             queryClient.invalidateQueries({ queryKey: ["posts"] });
         }
     });
+
+    useEffect(() => {
+        const options = {
+            root: null,
+            rootMargin: "0px",
+            threshold: 0.01
+        };
+        const observer = new IntersectionObserver((entries) => {
+            if (entries[0].isIntersecting && hasNextPage) {
+                fetchNextPage();
+            }
+        }, options);
+
+        const curr = ref.current;
+        if (curr) {
+            observer.observe(curr);
+        }
+
+        function cleanup() {
+            if (curr) {
+                observer.unobserve(curr);
+            }
+        }
+
+        return cleanup;
+    }, [hasNextPage]);
 
     function handleSubmit(e: React.FormEvent) {
         e.preventDefault();
@@ -47,7 +79,6 @@ export function Posts({
     return (
         <div>
             <h1>Posts</h1>
-
             <form onSubmit={handleSubmit}>
                 <div>
                     <input
@@ -66,30 +97,37 @@ export function Posts({
                 </div>
                 <button type="submit">Create Post</button>
             </form>
-
             <ul>
-                {posts?.map((post) => (
-                    <li key={post.id}>
-                        <a
-                            onClick={() => setPostId(post.id)}
-                            href="#"
-                            style={
-                                // We can access the query data here to show
-                                // bold links for ones that are cached
-                                queryClient.getQueryData(["post", post.id])
-                                    ? {
-                                          fontWeight: "bold",
-                                          color: "green"
-                                      }
-                                    : {}
-                            }
-                        >
-                            <h3>{post.title}</h3>
-                        </a>
-                        <p>{post.body}</p>
-                    </li>
-                ))}
+                {data?.pages.map((page) =>
+                    page.posts.map((post) => (
+                        <li key={post.id}>
+                            <a
+                                onClick={() => setPostId(post.id)}
+                                href="#"
+                                style={
+                                    // We can access the query data here to show
+                                    // bold links for ones that are cached
+                                    queryClient.getQueryData([
+                                        "post",
+                                        post.id
+                                    ])
+                                        ? {
+                                              fontWeight: "bold",
+                                              color: "green"
+                                          }
+                                        : {}
+                                }
+                            >
+                                <h3>{post.title}</h3>
+                            </a>
+                            <p>{post.body}</p>
+                        </li>
+                    ))
+                )}
             </ul>
+
+            {/* element that triggers refetching when visible */}
+            <div ref={ref}></div>
         </div>
     );
 }
--- src/data/data.ts
+++ src/data/data.ts
@@ -42,16 +42,186 @@ let posts: Post[] = [
         title: "Mock Post 7",
         body: "This is the body of Mock Post 7",
         userId: 6
+    },
+    {
+        id: 8,
+        title: "Mock Post 8",
+        body: "This is the body of Mock Post 8",
+        userId: 7
+    },
+    {
+        id: 9,
+        title: "Mock Post 9",
+        body: "This is the body of Mock Post 9",
+        userId: 8
+    },
+    {
+        id: 10,
+        title: "Mock Post 10",
+        body: "This is the body of Mock Post 10",
+        userId: 9
+    },
+    {
+        id: 11,
+        title: "Mock Post 11",
+        body: "This is the body of Mock Post 11",
+        userId: 10
+    },
+    {
+        id: 12,
+        title: "Mock Post 12",
+        body: "This is the body of Mock Post 12",
+        userId: 11
+    },
+    {
+        id: 13,
+        title: "Mock Post 13",
+        body: "This is the body of Mock Post 13",
+        userId: 12
+    },
+    {
+        id: 14,
+        title: "Mock Post 14",
+        body: "This is the body of Mock Post 14",
+        userId: 13
+    },
+    {
+        id: 15,
+        title: "Mock Post 15",
+        body: "This is the body of Mock Post 15",
+        userId: 14
+    },
+    {
+        id: 16,
+        title: "Mock Post 16",
+        body: "This is the body of Mock Post 16",
+        userId: 15
+    },
+    {
+        id: 17,
+        title: "Mock Post 17",
+        body: "This is the body of Mock Post 17",
+        userId: 16
+    },
+    {
+        id: 18,
+        title: "Mock Post 18",
+        body: "This is the body of Mock Post 18",
+        userId: 17
+    },
+    {
+        id: 19,
+        title: "Mock Post 19",
+        body: "This is the body of Mock Post 19",
+        userId: 18
+    },
+    {
+        id: 20,
+        title: "Mock Post 20",
+        body: "This is the body of Mock Post 20",
+        userId: 19
+    },
+    {
+        id: 21,
+        title: "Mock Post 21",
+        body: "This is the body of Mock Post 21",
+        userId: 20
+    },
+    {
+        id: 22,
+        title: "Mock Post 22",
+        body: "This is the body of Mock Post 22",
+        userId: 21
+    },
+    {
+        id: 23,
+        title: "Mock Post 23",
+        body: "This is the body of Mock Post 23",
+        userId: 22
+    },
+    {
+        id: 24,
+        title: "Mock Post 24",
+        body: "This is the body of Mock Post 24",
+        userId: 23
+    },
+    {
+        id: 25,
+        title: "Mock Post 25",
+        body: "This is the body of Mock Post 25",
+        userId: 24
+    },
+    {
+        id: 26,
+        title: "Mock Post 26",
+        body: "This is the body of Mock Post 26",
+        userId: 25
+    },
+    {
+        id: 27,
+        title: "Mock Post 27",
+        body: "This is the body of Mock Post 27",
+        userId: 26
+    },
+    {
+        id: 28,
+        title: "Mock Post 28",
+        body: "This is the body of Mock Post 28",
+        userId: 27
+    },
+    {
+        id: 29,
+        title: "Mock Post 29",
+        body: "This is the body of Mock Post 29",
+        userId: 28
+    },
+    {
+        id: 30,
+        title: "Mock Post 30",
+        body: "This is the body of Mock Post 30",
+        userId: 29
+    },
+    {
+        id: 31,
+        title: "Mock Post 31",
+        body: "This is the body of Mock Post 31",
+        userId: 30
+    },
+    {
+        id: 32,
+        title: "Mock Post 32",
+        body: "This is the body of Mock Post 32",
+        userId: 31
     }
 ];
 
-export async function fetchMockPosts(): Promise<Array<Post>> {
+type FetchPostsReturn = {
+    posts: Post[];
+    nextIdx: number;
+};
+
+async function fetchMockPosts({
+    pageParam
+}: {
+    pageParam: number;
+}): Promise<FetchPostsReturn> {
     return new Promise((resolve) => {
-        setTimeout(() => resolve(posts), 500);
+        var startingIdx = pageParam * 5;
+        setTimeout(
+            () =>
+                resolve({
+                    posts: posts.slice(startingIdx, startingIdx + 5),
+                    nextIdx:
+                        posts.length - 1 >= startingIdx + 5
+                            ? pageParam + 1
+                            : -1
+                }),
+            500
+        );
     });
 }
 
-export async function createMockPost(post: Post): Promise<Post> {
+async function createMockPost(post: Post): Promise<Post> {
     return new Promise((resolve) => {
         setTimeout(() => {
             posts.push(post);
@@ -60,7 +230,7 @@ export async function createMockPost(post: Post): Promise<Post> {
     });
 }
 
-export function getPostById(id: number): Promise<Post | undefined> {
+function getPostById(id: number): Promise<Post | undefined> {
     return new Promise((resolve) => {
         setTimeout(() => {
             const post = posts.find((post) => post.id === id);
@@ -68,3 +238,5 @@ export function getPostById(id: number): Promise<Post | undefined> {
         }, 1000);
     });
 }
+
+export { fetchMockPosts, getPostById, createMockPost };
