252d328af7d46893610561bca8fd5e770eef69cd
Akilbek <akilbekurinbaev0616@gmail.com>
2024-09-30 19:17:02 +0500

    Implemented useInfiniteQuery

--- src/data.ts
+++ src/data.ts
@@ -5,32 +5,89 @@ export type Post = {
   userId: number
 }
 
-// Initial mock posts
 let posts: Post[] = [
   {
-    id: 1,
-    title: 'Mock Post 1',
-    body: 'This is the body of Mock Post 1',
-    userId: 1,
+    "id": 1,
+    "title": "Mock Post 1",
+    "body": "This is the body of Mock Post 1",
+    "userId": 1
   },
   {
-    id: 2,
-    title: 'Mock Post 2',
-    body: 'This is the body of Mock Post 2',
-    userId: 1,
+    "id": 2,
+    "title": "Mock Post 2",
+    "body": "This is the body of Mock Post 2",
+    "userId": 1
   },
   {
-    id: 3,
-    title: 'Mock Post 3',
-    body: 'This is the body of Mock Post 3',
-    userId: 2,
+    "id": 3,
+    "title": "Mock Post 3",
+    "body": "This is the body of Mock Post 3",
+    "userId": 2
   },
+  {
+    "id": 4,
+    "title": "Mock Post 4",
+    "body": "This is the body of Mock Post 4",
+    "userId": 2
+  },
+  {
+    "id": 5,
+    "title": "Mock Post 5",
+    "body": "This is the body of Mock Post 5",
+    "userId": 3
+  },
+  {
+    "id": 6,
+    "title": "Mock Post 6",
+    "body": "This is the body of Mock Post 6",
+    "userId": 3
+  },
+  {
+    "id": 7,
+    "title": "Mock Post 7",
+    "body": "This is the body of Mock Post 7",
+    "userId": 4
+  },
+  {
+    "id": 8,
+    "title": "Mock Post 8",
+    "body": "This is the body of Mock Post 8",
+    "userId": 4
+  },
+  {
+    "id": 9,
+    "title": "Mock Post 9",
+    "body": "This is the body of Mock Post 9",
+    "userId": 5
+  },
+  {
+    "id": 10,
+    "title": "Mock Post 10",
+    "body": "This is the body of Mock Post 10",
+    "userId": 5
+  },
+  {
+    "id": 11,
+    "title": "Mock Post 11",
+    "body": "This is the body of Mock Post 11",
+    "userId": 6
+  },
+  {
+    "id": 12,
+    "title": "Mock Post 12",
+    "body": "This is the body of Mock Post 12",
+    "userId": 6
+  }
 ]
 
 // Simulate fetching all posts
-export const fetchMockPosts = async (): Promise<Array<Post>> => {
+export const fetchMockPosts = async (page = 1, limit=5): Promise<Array<Post>> => {
   return new Promise((resolve) => {
-    setTimeout(() => resolve(posts), 500) // Simulate network latency
+    setTimeout(() => {
+        const start = (page-1)*limit;
+        const end = start + limit;
+        const paginatedPosts = posts.slice(start,end);
+      resolve(paginatedPosts)}, 500) // Simulate network latency
   })
 }
 
